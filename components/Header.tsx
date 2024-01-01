f9d2add658bc913e7e20041b9c50a56a7d6970a0
4bh16am <120167005+4bh1zd@users.noreply.github.com>
2024-01-01 18:18:48 +0530

    header added

--- components/Header.tsx
+++ components/Header.tsx
@@ -1,13 +1,32 @@
-import Link from 'next/link'
-import React from 'react'
+import { navLinks } from "@/constats";
+import Link from "next/link";
+import React from "react";
+import NavLink from "./NavLink";
+import { Phone } from "lucide-react";
 
 const Header = () => {
-  return (<header>
-    <Link href={"/"}>
-        <h5 className='text-purple text-xl font-semibold'>S Abhiram.</h5>
-    </Link>
-    </header>);
-  
-}
+  return (
+    <header className="flex items-center justify-between h-20 max-width border-b border-peach">
+      <Link href={"/"}>
+        <h5 className="text-purple text-xl font-semibold">S Abhiram.</h5>
+      </Link>
+      <nav className="flex items-center gap-12">
+        <ul className="flex items-center gap-8">
+          {navLinks.map(
+            ((link, index) => (
+              <li key={index}>
+                < NavLink href={link.url} label={link.label} />
+              </li>
+            ))
+          )}
+        </ul>
+        <Link href={"/contact"} className="btn btn-primary">
+            <Phone size={24}/>
+            Contact me
+        </Link>
+      </nav>
+    </header>
+  );
+};
 
-export default Header
+export default Header;
